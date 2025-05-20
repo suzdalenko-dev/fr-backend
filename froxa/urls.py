ea9a19aacd34f7c5e0ba56735b230527fd6bcedf
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2025-05-20 13:42:07 +0200

    suzdalenko

--- froxa/urls.py
+++ froxa/urls.py
@@ -5,7 +5,14 @@ from django.urls import path
 def api_test(request):
     return JsonResponse({"mensaje": "Django OK 2"})
 
+def zzircon_fun(request, actionx, code, description):
+    return JsonResponse({
+        "action": actionx,
+        "code": code,
+        "description": description
+    })
+
 urlpatterns = [
-    path('test/', api_test),
+    path('zzircon/<str:actionx>/<str:code>/<str:description>/', zzircon_fun),
     path('test2/', api_test),
 ]
